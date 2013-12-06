3db1695a2642a8f1cdd029a58c15e4135f7fcfe2
Mike Chen <chintown@gmail.com>
2013-12-06 16:27:24 +0800

    add v4 uuid generator

--- common/crypt.php
+++ common/crypt.php
@@ -59,3 +59,27 @@
         $salt = substr($crypted_w_meta, 60);
         return array('crypted' => $crypted, 'salt' => $salt);
     }
+
+    // ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+    // http://stackoverflow.com/questions/2040240/php-function-to-generate-v4-uuid
+    function gen_v4_uuid() {
+        return sprintf( '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+            // 32 bits for "time_low"
+            mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
+
+            // 16 bits for "time_mid"
+            mt_rand( 0, 0xffff ),
+
+            // 16 bits for "time_hi_and_version",
+            // four most significant bits holds version number 4
+            mt_rand( 0, 0x0fff ) | 0x4000,
+
+            // 16 bits, 8 bits for "clk_seq_hi_res",
+            // 8 bits for "clk_seq_low",
+            // two most significant bits holds zero and one for variant DCE1.1
+            mt_rand( 0, 0x3fff ) | 0x8000,
+
+            // 48 bits for "node"
+            mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff )
+        );
+    }
