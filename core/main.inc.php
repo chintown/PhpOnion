f97d4ebf8ff6ed28f9e4be9e3099e4d56db7b4f5
Mike Chen <chintown@gmail.com>
2013-12-17 15:34:50 +0800

    CRITICAL. update main.inc

--- core/main.inc.php
+++ core/main.inc.php
@@ -1,9 +1,9 @@
 <?php
 
-//$project_path = realpath(dirname(__FILE__).'/../');
-//ini_set('include_path', ini_get('include_path') . ':' . $project_path);
-//// var_dump(ini_get('include_path'));
+// this file setup the proper include path and the very basic critical files
+// no execution logic
 
+// need to SET/override the following required files
 require 'config/dev.php';
 require 'config/path.php';
 
@@ -11,4 +11,3 @@ $project_path = realpath(dirname(__FILE__).'/../');
 ini_set('include_path', ini_get('include_path') . ':' . $project_path);
 
 require 'common/std.php';
-require 'core/controller.php';
--- entry/index.php
+++ entry/index.php
@@ -1,2 +1,7 @@
 <?php
-    require('../common/main.inc.php');
+
+// the controller will provide the simple routing/chaining mechanism
+// out-of-box
+
+require('../core/main.inc.php');
+require 'core/controller.php';
