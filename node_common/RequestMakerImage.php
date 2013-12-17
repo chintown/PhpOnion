5d7e162ed186be063d82257da469aa068488e4cd
Mike Chen <chintown@gmail.com>
2013-12-17 17:04:49 +0800

    MAJOR. change routing of image entry only for GET action

--- node_common/RequestMakerImage.php
+++ node_common/RequestMakerImage.php
@@ -7,14 +7,21 @@ class RequestMakerImage extends RequestMaker {
     }
     protected function parseParamsBy($verb) {
         $params = array();
-        $parts = explode('/', $_GET['target'], 2);
-        if (count($parts) === 2 && !empty($parts[1])) {
-            $file_info = explode('/', $parts[1]);
-            $params['filename'] = $file_info[0];
-            $dimension = (count($file_info) === 2) ? $file_info[1] : '0x0'; // serve as original dimension
-            list($params['thumb_w'], $params['thumb_h']) = explode('x', $dimension);
-            $params['thumb_w'] = intval($params['thumb_w']);
-            $params['thumb_h'] = intval($params['thumb_h']);
+        switch ($verb) {
+            case 'GET':
+                $parts = explode('/', $_GET['target'], 2);
+                if (count($parts) === 2 && !empty($parts[1])) {
+                    $file_info = explode('/', $parts[1]);
+                    $params['filename'] = $file_info[0];
+                    $dimension = (count($file_info) === 2) ? $file_info[1] : '0x0'; // serve as original dimension
+                    list($params['thumb_w'], $params['thumb_h']) = explode('x', $dimension);
+                    $params['thumb_w'] = intval($params['thumb_w']);
+                    $params['thumb_h'] = intval($params['thumb_h']);
+                }
+                break;
+            default:
+                $params = parent::parseParamsBy($verb);
+                break;
         }
         return $params;
     }
