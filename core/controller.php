0e507e4ef004f9cc5f23049be746fd6cebd785ee
Mike Chen <chintown@gmail.com>
2013-12-17 15:03:11 +0800

    rewrite controller and main.inc

--- core/controller.php
+++ core/controller.php
@@ -1,96 +1,100 @@
 <?php
-    /*
-    - load config
-    - include and initiate node
-    - start chain
 
-    0. set up request object
-    1. decide chain
-    2. find node
-    3. invoke chain
-    4. call entry of node
-    5. set up response content
-    6. output response
+/*
+- load config
+- include and initiate node
+- start chain
 
-    - checking hooks on nodes
-    - error handling
-    */
+0. set up request object
+1. decide chain
+2. find node
+3. invoke chain
+4. call entry of node
+5. set up response content
+6. output response
 
-    // load service configuration
-    $cwd = dirname(__FILE__).'/';
+- checking hooks on nodes
+- error handling
+*/
+
+$cwd = dirname(__FILE__).'/';
+require $cwd . 'BaseNode.php';
+require $cwd . 'Response.php';
+require $cwd . '../common/RestUtil.php';
+define('BASE_NODE_REPO', $cwd.'../node_common/');
+define('BUSINESS_NODE_REPO', FOLDER_ROOT.'/node_business/');
+
+$services = load_services_manifest($cwd);
+$entry = parse_target_entry_name($_GET['target'], $services)
+                or die("Error: invalid routing entry: [".$_GET['target']."]");
+list($main_chain_nodes, $sub_chain_nodes) = load_services_nodes($services, $entry);
+$chain_nodes = array_merge($main_chain_nodes, $sub_chain_nodes);
+$node_paths = find_node_paths($chain_nodes, $error) or die($error);
+$node_instance = init_node_instances($node_paths, $error) or die($error);
+
+// prepare req/res and invoke chain
+$req = null;
+$res = new Response();
+$node_instance[0]->execute($req, $res);
+
+// -----------------------------------------------------------------------------
+
+function load_services_manifest($cwd) {
     require $cwd . '../lib/spyc/spyc.php';
-    $conf = spyc_load_file(FOLDER_ROOT.'config/services.yaml');
+    $conf = spyc_load_file(FOLDER_ROOT . 'config/services.yaml');
+    return $conf;
+}
 
-    // load base class
-    require $cwd.'BaseNode.php';
-    $parts = explode('/', $_GET['target']); // check .htaccess
-    define('SUB_CHAIN', $parts[0]);
-    if (SUB_CHAIN === '') {
-        return;
-    } else if (!isset($conf[SUB_CHAIN])) {
-        die("Error invalid route: ".SUB_CHAIN);
+function parse_target_entry_name($route_path, $services) {
+    $parts = explode('/', $route_path); // check .htaccess
+    $entry = $parts[0];
+    if ($entry === '') {
+        return false;
+    } else if (!isset($services[$entry])) {
+        return false;
+    } else {
+        return $entry;
     }
+}
 
-    // extract sub config
-    $conf_main = (isset($conf[SUB_CHAIN.'_chain']))
-                    ? $conf[SUB_CHAIN.'_chain']
-                    : $conf['default_chain'];
-    $conf_sub_chain = $conf[SUB_CHAIN];
+function load_services_nodes($services, $entry) {
+    $main_chain_nodes = (isset($services[$entry.'_chain']))
+        ? $services[$entry.'_chain']
+        : $services['default_chain'];
+    $sub_chain_nodes = $services[$entry];
+    return array($main_chain_nodes, $sub_chain_nodes);
+}
 
-    // initiate chain
-    $default_instances = init_node_instances($conf_main);
-    if (count($default_instances) === 0) {
-        die("Error can not initiate default chain. ".var_export($conf_main));
-    }
-    $default_head = $default_instances[0];
-    $default_tail = $default_instances[count($default_instances) - 1];
-
-    $sub_chain_instances = init_node_instances($conf_sub_chain);
-    if (count($sub_chain_instances) === 0) {
-        die("Error can not initiate sub_chain chain. ".var_export($conf_sub_chain));
-    }
-    $sub_chain_head = $sub_chain_instances[0];
-
-    $default_tail->setNext($sub_chain_head);
-
-    // prepare req/res
-    require $cwd . "../common/RestUtil.php";
-    require $cwd . "../common/Response.php";
-
-    $req = null;
-    $res = new Response();
-
-    // invoke chain
-    $default_head->execute($req, $res);
-
-
-    function init_node_instances($names) {
-        global $cwd;
-        $prev_name= null;
-        $prev_instance = null;
-        $instances = array();
-        foreach ($names as $name) {
-            // echo "start init $name \n";
-            $node_file = $cwd.'../node_common/'.$name.'.php';
+function find_node_paths($node_names, &$error) {
+    $node_paths = array();
+    foreach ($node_names as $name) {
+        $node_file = BASE_NODE_REPO . $name . '.php';
+        if (file_exists($node_file)) {
+            $node_paths[$name] = $node_file;
+        } else {
+            $node_file = BUSINESS_NODE_REPO . $name . '.php';
             if (file_exists($node_file)) {
-                include $node_file;
+                $node_paths[$name] = $node_file;
             } else {
-                $node_file = FOLDER_ROOT.'/node_business/'.$name.'.php';
-                if (file_exists($node_file)) {
-                    include $node_file;
-                } else {
-                    die("Error node ($name) not found. $node_file");
-                }
+                $error = "Error: node ($name) not found. $node_file";
+                return false;
             }
-            $instance = new $name;
-            if (isset($prev_instance)) {
-                $prev_instance->setNext($instance);
-            }
-            // echo "after meet $name we update $prev_name \n";var_dump($prev_instance);
-            $instances[] = $instance;
-
-            $prev_name = $name;
-            $prev_instance = $instance;
         }
-        return $instances;
     }
+    return $node_paths;
+}
+
+function init_node_instances($node_paths, &$error) {
+    $prev_instance = null;
+    $instances = array();
+    foreach ($node_paths as $name => $path) {
+        require $path;
+        $instance = new $name;
+        if (isset($prev_instance)) {
+            $prev_instance->setNext($instance);
+        }
+        $instances[] = $instance;
+        $prev_instance = $instance;
+    }
+    return $instances;
+}
--- core/main.inc.php
+++ core/main.inc.php
@@ -1,7 +1,6 @@
 <?php
 
-$cwd = dirname(__FILE__).'/';
-require $cwd.'../config/dev.php';
-require $cwd.'../config/path.php';
-require $cwd . '../common/std.php';
-require $cwd . 'controller.php';
+$project_path = realpath(dirname(__FILE__).'/../');
+ini_set('include_path', ini_get('include_path') . ':' . $project_path);
+
+// TODO add sample
