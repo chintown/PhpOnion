3cf4ca61a76740fd779e921c305d78353c6dc671
Mike Chen <chintown@gmail.com>
2013-12-31 19:21:09 +0800

    add validation tool for routing config

---
+++ tool/_validate_routes.php
@@ -0,0 +1,7 @@
+<?php
+// copy from PhpOnion/tool/_validate_routes.php
+$pwd = dirname(__FILE__).'/';
+require $pwd.'../core/main.inc.php';
+require "core/Router.php";
+$r = new Router($pwd."../config/routes.yaml");
+$r->testAll();
