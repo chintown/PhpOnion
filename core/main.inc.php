c1e86cd492fac7dddb638e010a80fe08822d441d
Mike Chen <chintown@gmail.com>
2014-07-04 16:53:52 +0800

    allow forking from project PhpOnion

--- core/main.inc.php
+++ core/main.inc.php
@@ -1,13 +1,28 @@
 <?php
+    // this file setup the proper include path and the very basic critical files
+    // no execution logic
 
-// this file setup the proper include path and the very basic critical files
-// no execution logic
+    /* for self-contain testing only */
+    /**
+    $project_path = realpath(dirname(__FILE__).'/../');
+    ini_set('include_path', ini_get('include_path') . ':' . $project_path);
+    //*/
 
-// need to SET/override the following required files
-require 'config/dev.php';
-require 'config/path.php';
+    /**/ //__PARENT_PROJECT__
+    require 'config/dev.php';
+    if (DEV_MODE) {ini_set('apc.enabled',0);}
+    require 'config/prerequisite.php';
 
-$project_path = realpath(dirname(__FILE__).'/../');
-ini_set('include_path', ini_get('include_path') . ':' . $project_path);
+    $project_path = realpath(dirname(__FILE__).'/../');
+    ini_set('include_path', ini_get('include_path') . ':' . $project_path);
 
-require 'common/std.php';
+    require 'config/path.php';
+    require 'common/std.php';
+    //*/
+
+    /** //__CHILD_PROJECT__
+    $project_path = realpath(dirname(__FILE__).'/../');
+    ini_set('include_path', ini_get('include_path') . ':' . $project_path);
+    // var_dump(ini_get('include_path'));
+    require '/Users/chintown/src/php/PhpOnion/' . 'core/main.inc.php'; // __PARENT_ROOT__
+    //*/
