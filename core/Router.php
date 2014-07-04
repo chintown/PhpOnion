6902695190d6a51491384b193ccf09337281674b
Mike Chen <chintown@gmail.com>
2014-07-04 17:02:28 +0800

    fix filename

---
+++ core/Router.php
@@ -0,0 +1,116 @@
+<?php
+
+require_once 'lib/spyc/spyc.php';
+
+class Router {
+    var $routes; // pattern => entry
+    var $testCases;
+
+    const REGEX_ANY = "[^/]+";
+    const REGEX_INT = "[0-9]+";
+    const REGEX_ALPHA = "[a-zA-Z_-]+";
+    const REGEX_ALPHANUMERIC = "[0-9a-zA-Z_-]+";
+    const REGEX_STATIC = "%s";
+
+    public function __construct($file_or_array) {
+        $this->routes = array();
+        $this->testCases = array();
+
+        $route_manifest = (is_string($file_or_array))
+                            ? spyc_load_file($file_or_array)
+                            : $file_or_array;
+
+        foreach ($route_manifest as $entry => $profile) {
+            foreach ($profile['patterns'] as $pattern) {
+
+                $pattern = $this->convertPatternToRegexp($pattern);
+                $pattern = '&'.$pattern.'&';
+                if (!empty($this->routes[$pattern])) {
+                    die("duplicated routes in manifest: ". $pattern);
+                }
+
+                $this->routes[$pattern] = $entry;
+            }
+
+            if (isset($profile['samples'])) {
+                foreach ($profile['samples'] as $test) {
+                    $this->testCases[$test] = $entry;
+                }
+            }
+        }
+    }
+
+    public function parse($raw, &$rest_path_params) {
+        $matched_entry = null;
+        $matched_params = array();
+        foreach ($this->routes as $pattern => $entry) {
+//            de($pattern);
+//            de($raw);
+            preg_match($pattern, $raw, $matches);
+            if (empty($matches)) {
+                continue;
+            }
+            $matched_entry = $entry;
+            $matched_params = $this->filterMatchesWithNumericKey($matches);
+
+//            de($matches);
+//            de($matched_params);
+        }
+        $rest_path_params = $matched_params;
+        return $matched_entry;
+    }
+
+    public function testAll() {
+        foreach ($this->testCases as $case => $answer) {
+            $result = $this->parse($case, $params);
+            if ($result != $answer) {
+                die("mismatched route: \n".$case."\n".$answer."\n");
+            }
+        }
+
+        echo "OK\n";
+    }
+
+    private function filterMatchesWithNumericKey($matches) {
+        $result = array();
+        foreach($matches as $key => $val) {
+            if (!is_numeric($key)) {
+                $result[$key] = $val;
+            }
+        }
+        return $result;
+    }
+
+    private function convertPatternToRegexp($pattern) {
+        $parts = explode('/', $pattern);
+        $regexParts = map($parts, array($this, 'convertPartToRegexp'), false);
+        return implode('/', $regexParts);
+    }
+    public function convertPartToRegexp($part) {
+        $args = explode(':', $part);
+        if (count($args) < 2) { // has no shorthand in pattern. e.g. int:key_name
+            return $part;
+        }
+        $type = $args[0];
+        $name = $args[1];
+        $regexPart = '';
+        switch (strtolower($type)) {
+            case "int":
+            case "integer":
+                $regexPart = self::REGEX_INT;
+                break;
+            case "alpha":
+                $regexPart = self::REGEX_ALPHA;
+                break;
+            case "alphanumeric":
+            case "alphanum":
+            case "alnum":
+                $regexPart = self::REGEX_ALPHANUMERIC;
+                break;
+            default:
+                $regexPart = self::REGEX_ANY;
+                break;
+        }
+        return "(?P<$name>$regexPart)";
+    }
+}
