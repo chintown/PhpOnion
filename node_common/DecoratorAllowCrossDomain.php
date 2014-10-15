a42ebfcab30d6a79613dc6ccf043505206048343
Mike Chen <chintown@gmail.com>
2014-10-15 18:13:19 +0800

    add node for supporting cross-domain accessing

---
+++ node_common/DecoratorAllowCrossDomain.php
@@ -0,0 +1,14 @@
+<?php
+
+class DecoratorAllowCrossDomain extends BaseNode {
+    function __construct() {
+    }
+    public function execute(&$req, Response &$res) {
+        $res->addHeaders(array(
+            'Access-Control-Allow-Origin'=> '*',
+            'Access-Control-Allow-Methods'=> 'GET, PUT, POST, DELETE',
+            'Access-Control-Allow-Headers'=> 'Origin, X-Requested-With, Content-Type, Accept',
+        ));
+        $this->next($req, $res);
+    }
+}
